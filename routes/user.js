7792925c3528da0b4e8e63fe43bad623fbcfe91e
meomeok9 <hoannmfx17428@funix.edu.vn>
2023-03-02 13:23:35 +0700

    permission added

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/userCtrl");
+const authPage = require("../middleware/authPage");
 router.post("/signup", userCtrl.addNewUser);
 router.post("/checkEmail", userCtrl.checkEmail);
-router.post("/checkOut", userCtrl.addOrder);
-router.post("/getAllOrder", userCtrl.getAllOrder);
-router.post("/orderDetail", userCtrl.getOrderDetail);
-router.post("/saveCart", userCtrl.saveCart);
+router.post(
+  "/checkOut",
+  authPage(["admin", "counselors", "customer"]),
+  userCtrl.addOrder
+);
+router.post(
+  "/getAllOrder",
+  authPage(["admin", "counselors", "customer"]),
+  userCtrl.getAllOrder
+);
+router.post(
+  "/orderDetail",
+  authPage(["admin", "counselors", "customer"]),
+  userCtrl.getOrderDetail
+);
+router.post(
+  "/saveCart",
+  authPage(["admin", "counselors", "customer"]),
+  userCtrl.saveCart
+);
 
 module.exports = router;
